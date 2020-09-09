81f4146ca2e60eb5040f828dabc1a715f7186d9e
nshefeek <connectshefeek@gmail.com>
2020-09-09 12:04:44 +0400

    Login page updated

--- taskee/taskee/settings.py
+++ taskee/taskee/settings.py
@@ -138,6 +138,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static_local'),
+        os.path.join(BASE_DIR, 'static_local', 'registration'),
         ]
 
 STATICFILES_FINDERS = (
